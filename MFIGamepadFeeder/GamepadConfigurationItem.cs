1f41073616455b6dc3c311c1ce1b5fa212f6c0b9
Michał Mizera <axadiw@gmail.com>
2015-11-25 01:26:02 +0100

    Init commit

---
+++ MFIGamepadFeeder/GamepadConfigurationItem.cs
@@ -0,0 +1,22 @@
+﻿namespace MFIGamepadFeeder
+{
+    public enum GamepadItemType
+    {
+        Axis,
+        Button,
+        DPadUp,
+        DPadRight,
+        DPadDown,
+        DPadLeft,
+        Empty
+    }
+
+    public class GamepadConfigurationItem
+    {
+        public GamepadItemType Type { get; set; }
+        public bool? InvertAxis { get; set; }
+        public bool? ConvertAxis { get; set; }
+        public HID_USAGES? TargetUsage { get; set; }
+        public uint? TargetButtonId { get; set; }
+    }
+}
---
+++ MFIGamepadFeeder/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+using HidSharp;
+
+namespace MFIGamepadFeeder
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+       
+
+        private static void SetupGamepad()
+        {
+            var loader = new HidDeviceLoader();
+            Thread.Sleep(2000); // Give a bit of time so our timing below is more valid as a benchmark.
+
+            var stopwatch = new Stopwatch();
+            var deviceList = loader.GetDevices().ToArray();
+
+            var device = loader.GetDevices(null, null, null, "289a4b11e297").First();
+            if (device == null)
+            {
+                Console.WriteLine("Failed to open device.");
+                return;
+            }
+
+            HidStream stream;
+            if (!device.TryOpen(out stream))
+            {
+                Console.WriteLine("Failed to open device.");
+                return;
+            }
+
+
+
+            Gamepad gamepad = new Gamepad(device.MaxInputReportLength);
+
+            using (stream)
+            {
+                while (true)
+                {
+                    var bytes = new byte[device.MaxInputReportLength];                    
+                    int count;
+                    try
+                    {
+                        count = stream.Read(bytes, 0, bytes.Length);
+                    }
+                    catch (TimeoutException)
+                    {
+                        continue;
+                    }
+
+                    if (count > 0)
+                    {
+                        gamepad.Gamepad_NewState(bytes);
+                    }
+                }
+            }
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            SetupGamepad();
+        }
+    }
+}
